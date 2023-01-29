da17bd118c2e7590289baa54e8cd96bb6c318bc5
Kacper Żarna <kacper@192.168.1.3>
2023-01-29 09:40:53 +0100

    Kod 29.01.2023 9:40

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import HomePage from "./components/HomePage/HomePage";
 function App() {
   return (
     <div className="App">
       <Navbar />
+      {/* <HomePage /> */}
     </div>
   );
 }
 
 export default App;
+
+// 1. Stwórz komponent LoginPage i wyświetl go w app.tsx
---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Typography } from "@mui/material";
+const HomePage = () => {
+  return (
+    <>
+      <Typography
+        variant="h2"
+        align="center"
+        sx={{ fontSize: "2rem", mt: ".8rem" }}
+      >
+        Today's hottest news
+      </Typography>
+    </>
+  );
+};
+
+export default HomePage;
