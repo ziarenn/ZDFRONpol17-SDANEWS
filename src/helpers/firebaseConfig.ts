c674d8639b86c47211551c0a0c4e119820f85024
Kacper Żarna <kacper@192.168.1.10>
2023-02-11 11:02:00 +0100

    Kod 11.02.2023 11:00

---
+++ src/helpers/firebaseConfig.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6EFb-NuMvfsiURo2UrZJcJPrhKGuAjsE",
+  authDomain: "zdfronpol17-sdanews.firebaseapp.com",
+  projectId: "zdfronpol17-sdanews",
+  storageBucket: "zdfronpol17-sdanews.appspot.com",
+  messagingSenderId: "318808277194",
+  appId: "1:318808277194:web:788d226d357a2dbfc1f433",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
