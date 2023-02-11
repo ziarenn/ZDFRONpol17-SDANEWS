9441266600af09c8e012c78a07f8d961a9fa7b65
Kacper Żarna <kacper@192.168.1.10>
2023-02-11 14:57:36 +0100

    Kod 11.02.2023 15:00

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,44 @@ import LoginPage from "./components/LoginPage/LoginPage";
 import RegisterForm from "./components/RegisterForm/RegisterForm";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CustomerForm from "./components/CustomerForm/CustomerForm";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./helpers/firebaseConfig";
+import { authContext } from "./helpers/authContext";
 
+/// CONTEXT API
+// useContext (hook) jest częścią całego narzędzie Context API
+// oprócz useContext mamy do dyspozycji funkcje createContext i komponent ContextProvider
+
+// SETUP CONTEXT API
+// 1. Stwórzenie contextu poprzez wywołanie funkcji createContext w osobnym pliku.
+// 2. Obwinięcie danej części aplikacji (lub całej aplikacji) providerem.
+// 3. Przekazanie danych do provider'a
 function App() {
-  const [x, setX] = useState();
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+  });
 
   return (
     <div className="App">
-      {/* {x ? <span>X</span> : null} */}
-      {/* {x && <span>X</span>} */}
       <BrowserRouter>
-        {/* STATIC */}
-        <Navbar />
-        <Routes>
-          {/* DYNAMIC */}
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterForm />} />
-          <Route path="/customer" element={<CustomerForm />} />
-        </Routes>
-        {/* STATIC */}
+        <authContext.Provider value={loggedIn}>
+          {/* STATIC */}
+          <Navbar />
+          <Routes>
+            {/* DYNAMIC */}
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterForm />} />
+            <Route path="/customer" element={<CustomerForm />} />
+          </Routes>
+          {/* STATIC */}
+        </authContext.Provider>
       </BrowserRouter>
     </div>
   );
