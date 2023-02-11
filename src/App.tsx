dc1dfe9202d0d210eea0732b1353b3d44a25cd69
Kacper Żarna <kacper@192.168.1.10>
2023-02-11 10:37:01 +0100

    Kod 11.02.2023 10:35

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import HomePage from "./components/HomePage/HomePage";
 import LoginPage from "./components/LoginPage/LoginPage";
 import RegisterForm from "./components/RegisterForm/RegisterForm";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
+  const [x, setX] = useState();
+
   return (
     <div className="App">
+      {/* {x ? <span>X</span> : null} */}
+      {/* {x && <span>X</span>} */}
       <BrowserRouter>
         {/* STATIC */}
         <Navbar />
@@ -23,3 +28,10 @@ function App() {
 }
 
 export default App;
+
+// 1. useEffect
+// 2. axios
+// 3. Renderowanie warunkowe
+// 4. Renderowanie w pętli
+// 5. useForm
+// 6. Routing
--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,13 +1,34 @@
 import React from "react";
 import { Button, Typography, TextField } from "@mui/material";
+import { useForm } from "react-hook-form";
+
+interface LoginFormValues {
+  email: string;
+  password: string;
+}
 
 const inputStyles = { display: "block", mx: "auto", my: ".5rem" };
 
 const LoginPage = () => {
+  const { register, handleSubmit } = useForm<LoginFormValues>();
+
+  const logUserIn = (data: LoginFormValues) => {};
+
   return (
-    <form style={{ display: "flex", flexDirection: "column" }}>
-      <TextField placeholder="email" sx={inputStyles} />
-      <TextField placeholder="password" sx={inputStyles} />
+    <form
+      style={{ display: "flex", flexDirection: "column" }}
+      onSubmit={handleSubmit(logUserIn)}
+    >
+      <TextField
+        placeholder="email"
+        sx={inputStyles}
+        {...register("email", { required: true })}
+      />
+      <TextField
+        placeholder="password"
+        sx={inputStyles}
+        {...register("password", { required: true })}
+      />
       <Button
         variant="contained"
         sx={{ display: "block", mb: "1rem", mx: "auto" }}
@@ -25,3 +46,5 @@ const LoginPage = () => {
 };
 
 export default LoginPage;
+
+// 1. Zastosuj useForm w  LoginPage.tsx, ściągnij dane z inputów email i password. Pamiętaj o wszystkich niezbędnych krokach.
