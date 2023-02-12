a4483c68f1f284d9e80dabd0ecfa5943c2a5d89b
Kacper Żarna <kacper@192.168.1.10>
2023-02-12 11:39:18 +0100

    Kod 12.02.2023 11:40

--- src/components/ProfilePhotoForm/ProfilePhotoForm.tsx
+++ src/components/ProfilePhotoForm/ProfilePhotoForm.tsx
@@ -1,6 +1,8 @@
 import { Button, Card, Typography } from "@mui/material";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { auth, storage } from "../../helpers/firebaseConfig";
+import { uploadBytes, ref } from "firebase/storage";
 
 interface ProfilePhotoFormValues {
   profilePhoto: FileList;
@@ -9,7 +11,20 @@ interface ProfilePhotoFormValues {
 const ProfilePhotoForm = () => {
   const { register, handleSubmit } = useForm<ProfilePhotoFormValues>();
 
-  const uploadPhoto = (data: ProfilePhotoFormValues) => {};
+  const uploadPhoto = (data: ProfilePhotoFormValues) => {
+    // 1. Stwórz zmienną photo, przypisz do niej OBIEKT reprezuntający sam plik (znajdziesz go gdzieś w obiekcie data z parametru).
+    const photo = data.profilePhoto[0];
+    // 2. Napisz ifa w którym sprawdzisz czy auth.currentUser istnieje, zaimportuj auth z firebaseConfig.ts
+    if (auth.currentUser) {
+      // WSZYSTKO PONIŻEJ W IFIE
+      // 3. Stwórz referencje do storagu, użyj do tego funkcji ref z firebase/storage. `/users/${auth.currentUser.uid}/profile`
+      const storageRef = ref(storage, `/users/${auth.currentUser.uid}/profile`);
+      // 4. Wywołanie funkcji uploadBytes (firebase/storage), pierwszy argument: ref z pkt 3, drugi argument: zmienna photo z pkt 1. Do wywołania przypnij thena, w którym będzie console.log('sukces')
+      uploadBytes(storageRef, photo)
+        .then(() => console.log("sukces"))
+        .catch((err) => console.error(err.message));
+    }
+  };
 
   return (
     <form onSubmit={handleSubmit(uploadPhoto)}>
--- src/helpers/firebaseConfig.ts
+++ src/helpers/firebaseConfig.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: "AIzaSyA6EFb-NuMvfsiURo2UrZJcJPrhKGuAjsE",
   authDomain: "zdfronpol17-sdanews.firebaseapp.com",
@@ -12,3 +12,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
